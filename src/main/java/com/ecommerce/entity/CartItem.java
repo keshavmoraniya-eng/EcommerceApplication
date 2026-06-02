45d9ddc7079b3357b44a20263c07861d50576eb2
keshavmoraniya-eng <keshavmoraniya1010@gmail.com>
2026-06-02 23:22:35 +0530

    Add CartItem Entity

---
+++ src/main/java/com/ecommerce/entity/CartItem.java
@@ -0,0 +1,34 @@
+package com.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "cart_item")
+@Getter
+@Setter
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id",nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public BigDecimal getSubTotal(){
+        return product.getPrice().multiply(BigDecimal.valueOf(quantity));
+    }
+
+}
